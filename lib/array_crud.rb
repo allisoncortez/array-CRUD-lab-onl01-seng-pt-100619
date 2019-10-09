39fd026fe8323d38d6f8b04e262d7780ae72c6e6
allison cortez <allizetorc@gmail.com>
2019-10-09 15:21:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,13 +21,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-    # new_array = []
   array.pop
-  # new_array
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
